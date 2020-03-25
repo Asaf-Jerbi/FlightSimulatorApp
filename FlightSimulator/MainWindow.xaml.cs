10f4a4c21bf41b4f9e996da2588d10acab654287
Roei Ben zeev <roeibenzeev@gmail.com>
2020-03-25 17:07:31 +0200

    Revert

--- FlightSimulator/MainWindow.xaml.cs
+++ FlightSimulator/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace FlightSimulator
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine("Roei");
+            Console.WriteLine("testing");
         }
     }
 }
