2e778ebcdba52f7cab89dab38e8885eb211b876a
Asaf-Jerbi <asaf.jerbi@gmail.com>
2020-03-25 14:30:49 +0200

    Adding button

--- FlightSimulator/MainWindow.xaml.cs
+++ FlightSimulator/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace FlightSimulator
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("btn clicked");
+        }
     }
 }
